38be39d7c14f0ae04cf49fd7c043dd09f460f153
floatingbug <sieber.thomas@yahoo.com>
2024-04-04 19:50:58 +0200

    package.json diff

---
+++ src/routes/addGuessedQuiz.js
@@ -0,0 +1,12 @@
+function addGuessedQuiz({store}){
+	return (req, res)=>{
+		requestHandler({req, res, store});
+	}
+}
+
+async function requestHandler({req, res, store}){
+ 	
+}
+
+
+module.exports = {addGuessedQuiz};
